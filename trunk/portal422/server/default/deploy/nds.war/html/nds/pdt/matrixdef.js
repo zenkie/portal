e56baba66805af6ece7ab14debd78d773895364c
jackrain <jackrain@df65ea39-c3a6-4049-895f-d46742369438>
2012-12-13 08:42:29 +0000

    支持art.diaglo
    git-svn-id: http://buildserver:9880/portal@787 df65ea39-c3a6-4049-895f-d46742369438

--- trunk/portal422/server/default/deploy/nds.war/html/nds/pdt/matrixdef.js
+++ trunk/portal422/server/default/deploy/nds.war/html/nds/pdt/matrixdef.js
@@ -258,7 +258,8 @@ MatrixDef.prototype = {
 		if (w ){
 			var iframe=w.document.getElementById("popup-iframe-0");
 			if(iframe){
-	    		w.setTimeout("Alerts.killAlert(document.getElementById('popup-iframe-0'));",1);
+	    		//w.setTimeout("Alerts.killAlert(document.getElementById('popup-iframe-0'));",1);
+	    		art.dialog.close();
 	    		return true;
 			}
 		}
