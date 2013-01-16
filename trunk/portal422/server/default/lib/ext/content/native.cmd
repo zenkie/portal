897705b8eb534d40d938bea463310a5938dbd6d4
jackrain <jackrain@df65ea39-c3a6-4049-895f-d46742369438>
2013-01-16 07:14:39 +0000

    git-svn-id: http://buildserver:9880/portal@941 df65ea39-c3a6-4049-895f-d46742369438

--- trunk/portal422/server/default/lib/ext/content/native.cmd
+++ trunk/portal422/server/default/lib/ext/content/native.cmd
@@ -12,18 +12,18 @@ native2ascii.exe -encoding GB2312 olap_zh_CN.properties.native olap_zh_CN.proper
 
 native2ascii.exe -encoding GBK olap_ja_JP.properties.native olap_ja_JP.properties
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_portalcontrol_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_portalcontrol_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_portalcontrol_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_portalcontrol_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_portletcontrol_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_portletcontrol_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_portletcontrol_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_portletcontrol_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_objcontrol_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_objcontrol_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_objcontrol_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_objcontrol_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_ahca_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_ahca_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_ahca_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_ahca_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_optioncontrol_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_optioncontrol_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_optioncontrol_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_optioncontrol_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_object_query_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_object_query_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_object_query_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_object_query_zh_CN.js
 
-native2ascii.exe -encoding GB2312 E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_querydef_zh_CN.native.js E:\portal\portal422\server\default\deploy\nds.war\html\nds\js\init_querydef_zh_CN.js
+native2ascii.exe -encoding GB2312 E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_querydef_zh_CN.native.js E:\newportal\portal422\server\default\deploy\nds.war\html\nds\js\init_querydef_zh_CN.js
 
 copy olap.properties.native olap.properties /Y
