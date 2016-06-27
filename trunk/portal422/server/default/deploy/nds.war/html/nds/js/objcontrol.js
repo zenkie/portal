3273d6ee29a16cf978d393ee283bc31c2a6bc1cd
jackrain <jackrsky@gmail.com>
2016-06-27 14:26:07 +0800

    修复提交没声音问题

--- trunk/portal422/server/default/deploy/nds.war/html/nds/js/objcontrol.js
+++ trunk/portal422/server/default/deploy/nds.war/html/nds/js/objcontrol.js
@@ -836,6 +836,7 @@ ObjectControl.prototype = {
 							evt.setUserData(result);
 							//application.dispatchEvent(evt);
 							if(evt.getUserData().data==undefined){
+								playAlert();
 								msgbox(result.message);
 							}else{
 								gc.updateGrid(evt);
